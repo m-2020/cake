02e40648a34486b4f7b96a69e990de46ffdfb93a
minami-taiga <373735656t@gmail.com>
2020-01-09 08:55:51 +0000

    会員登録画面　会員詳細画面　編集しました！！

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,2 +1,28 @@
 class Customers::CustomersController < ApplicationController
+
+def show
+	@customer = current_customer
+
+
+end
+
+def edit
+
+end
+
+def update
+
+end
+
+def change
+
+end
+
+def withdraw
+
+end
+
+
+
+
 end
