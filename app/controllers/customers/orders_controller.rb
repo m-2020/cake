370681ab715f5e1c1cd0ed16f1cae9724ef547f8
taishi352 <taishi352@gmail.com>
2020-01-16 05:03:16 +0000

    give up

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -5,8 +5,23 @@ class Customers::OrdersController < ApplicationController
 		@customer = current_customer
 	end
 
+	def judgement
+		@customer = current_customer
+		# @customer_address = @customer.address
+		@cart_products = current_customer.cart_products
+		@order = Order.new(order_params)
+	end
+
 	def create
-		
+		@order = Order.new(order_params)
+		@order.customer_id = current_customer.id
+		@order.save
+		redirect_to orders_thanks_path
+	end
+
+
+
+	def thanks
 	end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,12 +4,16 @@ class Order < ApplicationRecord
 
 	has_many :order_products, dependent: :destroy
 
-	enum payment:[:card, :bank]
 
 	enum sending_status:{入金待ち:0, 入金確認:1, 制作中:2, 発送準備中:3, 発送済:4}
 
 	def address_all_id
 		'〒' + self.post_code + ' ' + self.address + ' ' + self.last_name + ' ' + self.last_name
 	end
+
+	def payment_str
+        self.payment ? "クレジットカード" : "銀行振込"
+	end
+
 
 end
