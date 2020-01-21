28ff02e0450120ad6814db599772a2d63e263d85
yuyabanjou <yuyabanjou@gmail.com>
2020-01-21 06:04:32 +0000

    ステータス関係完成

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ApplicationRecord
 
 	enum payment: {クレジットカード: 0, 銀行振込: 1}
 
-	enum sending_status: {入金待ち: 0, 入金確認: 1, 制作中: 2, 発送準備中: 3, 発送済: 4}
+	enum sending_status: {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済: 4}
 
 	def address_all_id
 		'〒' + self.post_code + '/' + self.address + '/' + self.last_name + ' ' + self.last_name
