1f9a10661ce025164ccaca6447fcdec31b45a6fe
mkkoala <mako1105.96@gmail.com>
2020-01-21 03:03:14 +0000

    レイアウト更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_060723) do
+ActiveRecord::Schema.define(version: 2020_01_15_043702) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -34,14 +34,6 @@ ActiveRecord::Schema.define(version: 2020_01_15_060723) do
   end
 
   create_table "cart_products", force: :cascade do |t|
-    t.integer "customer_id"
-    t.integer "product_id"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "carts", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "product_id"
     t.integer "quantity"
@@ -81,23 +73,22 @@ ActiveRecord::Schema.define(version: 2020_01_15_060723) do
     t.integer "product_id"
     t.integer "quantity"
     t.integer "price"
-    t.integer "cook_status"
+    t.integer "cooking_status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id"
-    t.integer "post_code"
+    t.string "post_code"
     t.string "address"
     t.integer "postage"
     t.integer "billing_amount"
-    t.boolean "payment"
+    t.integer "payment"
     t.integer "sending_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.integer "address_id"
   end
 
   create_table "products", force: :cascade do |t|
